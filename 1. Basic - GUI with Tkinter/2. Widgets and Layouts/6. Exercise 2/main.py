55f7786c612544a6a2e7d11335ce6f7d49ad9a99
lepyavko.d <lepyavko.d@novaposhta.ua>
2022-12-07 13:14:19 +0200

    progress

--- 1. Basic - GUI with Tkinter/2. Widgets and Layouts/6. Exercise 2/main.py
+++ 1. Basic - GUI with Tkinter/2. Widgets and Layouts/6. Exercise 2/main.py
@@ -14,9 +14,26 @@ bottomFrame.pack()
 
 text1 = Label(topFrame, text="This application demonstrates frame layout")
 # TODO make your button objects here (all 9 of them!)
+button1 = Button(middleFrame1, text='button1', fg='black')
+button2 = Button(middleFrame1, text='button2', fg='black')
+button3 = Button(middleFrame1, text='button3', fg='black')
+button4 = Button(middleFrame2, text='button4', fg='black')
+button5 = Button(middleFrame2, text='button5', fg='black')
+button6 = Button(middleFrame2, text='button6', fg='black')
+button7 = Button(bottomFrame, text='button7', fg='black')
+button8 = Button(bottomFrame, text='button8', fg='black')
+button9 = Button(bottomFrame, text='button9', fg='black')
 
 text1.pack()
 # TODO pack your buttons into the window here
-
+button1.pack(side='left')
+button2.pack(side='left')
+button3.pack(side='right')
+button4.pack(side='left')
+button5.pack(side='left')
+button6.pack(side='right')
+button7.pack(side='left')
+button8.pack(side='left')
+button9.pack(side='right')
 
 window.mainloop()
