7693177995d3ecd50c581181f51e3213b9feda45
lepyavko.d <lepyavko.d@novaposhta.ua>
2022-12-12 11:47:53 +0200

    progress

--- 1. Basic - GUI with Tkinter/3. Commands with Buttons/5. Exercise 5/task.py
+++ 1. Basic - GUI with Tkinter/3. Commands with Buttons/5. Exercise 5/task.py
@@ -7,6 +7,7 @@ def clicked():
     name = name_entry.get()
     surname = surname_entry.get()
     # TODO create the welcome message and configure the text attribute of the message_label
+    message_label.config(text=f"Hello, {name} {surname}")
 
 
 # create window
@@ -16,16 +17,26 @@ window.geometry("300x150")
 
 # create widgets
 # TODO Create your widgets
+top_frame = Frame(window)
+middle_frame = Frame(window)
+bottom_frame = Frame(window)
+name_label = Label(top_frame, text="Name")
+surname_label = Label(middle_frame, text="Surname")
+name_entry = Entry(top_frame, text="Enter your name")
+surname_entry = Entry(middle_frame, text="Enter your surname")
+register_button = Button(bottom_frame, text="Register", command=clicked)
+message_label = Label(bottom_frame, text="")
 
 # pack widgets into window container
 top_frame.pack(fill="both")
 middle_frame.pack(fill="both")
+bottom_frame.pack(fill="both")
 name_label.pack(side=LEFT)
 name_entry.pack(side=RIGHT, fill="both", expand=True)
 surname_label.pack(side=LEFT)
 surname_entry.pack(side=LEFT, fill="both", expand=True)
-register_button.pack()
-message_label.pack()
+register_button.pack(side=TOP)
+message_label.pack(side=BOTTOM)
 
 # open window
 window.mainloop()
